19640373c6e2a963daac5d6bc7537986fb7bba06
Abdullah Ehsan <abdullahehsan4242@gmail.com>
2024-12-08 17:09:21 +0500

    patrol path fixed

--- Assets/Scripts/NPCs/DrGraves/PatrollingPath.cs
+++ Assets/Scripts/NPCs/DrGraves/PatrollingPath.cs
@@ -7,7 +7,13 @@ public class PatrollingPath : MonoBehaviour
     public List<Transform> nodes = new();
     void Start()
     {
-        
+        nodes.Clear();
+
+        Transform[] childNodes = GetComponentsInChildren<Transform>();
+        for (int i = 1; i < childNodes.Length; i++)
+        {
+            nodes.Add(childNodes[i]);
+        }
     }
 
     // Update is called once per frame
